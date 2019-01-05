2d4b5fcf6a7153d91536ffcec074efefcdb8459f
Jonathan Harper <jonathan@Jonathan-MBP.local>
2019-01-05 20:18:28 +0000

    Kinda works

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 
 WORD = 'Disputing'
-WORD_LIST = ['Computing', 'Polluting', 'Diluting', 'Commuting', 'Recruiting', 'Drooping']
+WORD_LIST = ['ooohooo', 'Computing', 'Polluting', 'Diluting', 'Commuting', 'Recruiting', 'Drooping']
 
 # The phonetic language in the form letters: phonetic sound.
 # Phonetic alphabet taken from http://www.antimoon.com/how/pronunc-soundsipa.htm
@@ -43,11 +43,11 @@ PHONETIC_ALPHABET = {
     'oy': '/ɔɪ/',
     'oi': '/ɔɪ/',
     'Chere': '/eə/',
-    'air': 'eə',
-    'ear': 'ɪə',
-    'ere': 'ɪə',
-    'ure': 'ʊə',
-    'ourA': 'ʊə',
+    'air': '/eə/',
+    'ear': '/ɪə/',
+    'ere': '/ɪə/',
+    'ure': '/ʊə/',
+    'ourA': '/ʊə/',
 
     # Consonants
     'b': '/b/',
@@ -61,37 +61,68 @@ PHONETIC_ALPHABET = {
     'l': '/l/',
     'm': '/m/',
     'n': '/n/',
-    'ing': 'ŋ',
+    'ing': '/ŋ/',
     'p': '/p/',
     'r': '/r/',
     's': '/s/',
     'ss': '/s/',
-    'sh': 'ʃ',
+    'sh': '/ʃ/',
     't': '/t/',
     'tt': '/t/',
-    'ch': 'tʃ',
-    'thin': 'θ',
-    'oth': 'θ',
-    'th': 'ð',
-    'v': 'v',
-    'w': 'w',
-    'z': 'z',
-    'eas': 'ʒ',
-    'is': 'ʒ',
-    'j': 'dʒ',
-    'ge': 'dʒ'
+    'ch': '/tʃ/',
+    'thin': '/θ/',
+    'oth': '/θ/',
+    'th': '/ð/',
+    'v': '/v/',
+    'w': '/w/',
+    'z': '/z/',
+    'eas': '/ʒ/',
+    'is': '/ʒ/',
+    'j': '/dʒ/',
+    'ge': '/dʒ/',
+
+
+    # testing
+    'Comp': '/yassss/',
+    # 'ooo': '/yes/',
+    'Soo': '/hello/'
 }
+
+CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+VOWELS = 'aeiou'
+
+
+def check_if_mod_valid(char, modifier):
+    if modifier == 'C':
+        if char in CONSONANTS:
+            return True
+    elif modifier == 'V':
+        if char in VOWELS:
+            return True
+    elif char == modifier:
+        return True
+    return False
 
 
 def split_words_into_syllables(target_word, word_list):
+    phonetic_word_list = []
     # We are fitting the word to the phonetics to the fullest extent, such that largest phonetics are fitted first
     phonetic_largest = sorted([x for x in PHONETIC_ALPHABET], key=len, reverse=True)
 
     for word in word_list:
+        word = word.lower()
+        phonetic_word = word
         print()
         print(word)
         # Checking what exists (MOVE THIS TO OUTER LOOP)
         for phonetic in phonetic_largest:
+
+            settt = set(word)
+
+            # No further chars left
+            if set(word) == {'*'}:
+                phonetic_word_list.append(phonetic_word)
+                break
 
             # If the phonetic contains a modifier
 
@@ -99,20 +130,42 @@ def split_words_into_syllables(target_word, word_list):
             for i in range(len(phonetic)):
                 char = phonetic[i]
                 if char.isupper():
+                    # True if modifier at start of phonetic, false if at end (or otherwise)
                     if i == 0:
                         modifiers[char] = True
                     else:
                         modifiers[char] = False
 
-            for char in modifiers.values():
+            for char in modifiers:
                 phonetic = phonetic.replace(char, '')
 
             if phonetic in word:
                 substr_index = word.index(phonetic)
                 if modifiers:
                     for mod in modifiers:
-                        modifiers[mod]
-                    print(phonetic)
+                        if modifiers[mod]:
+                            # If modifier is S
+                            if mod == 'S':
+                                char_in_mod_pos = word[0]
+                            else:
+                                char_in_mod_pos = word[substr_index - 1]
+                        else:
+                            # If modifier is E
+                            if mod == 'E':
+                                char_in_mod_pos = word[-1]
+                            else:
+                                char_in_mod_pos = word[substr_index + len(mod)]
+                        if check_if_mod_valid(char_in_mod_pos, mod):
+                            # Replaces the substring with * in the word, and replaces the substring with the
+                            # phonetic representation in the phonetic_word
+                            word = word.replace(char_in_mod_pos + phonetic, '*')
+                            phonetic_word = phonetic_word.replace(char_in_mod_pos + phonetic,
+                                                                  PHONETIC_ALPHABET[mod + phonetic])
+                else:
+                    word = word.replace(phonetic, '*')
+                    phonetic_word = phonetic_word.replace(phonetic, PHONETIC_ALPHABET[phonetic])
+
+        phonetic_word_list.append(phonetic_word)
 
     # print(phonetic_largest)
     pass
